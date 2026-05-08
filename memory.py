7f61237cde1696bfdafbcefd5ce1cc6841a9d3fc
Alex-cmd223 <a01667608@tec.mx>
2026-05-08 11:34:36 -0600

    Grid 4x4

--- memory.py
+++ memory.py
@@ -15,9 +15,9 @@ from turtle import *
 from freegames import path
 
 car = path('car.gif')
-tiles = list(range(32)) * 2
+tiles = list(range(8)) * 2
 state = {'mark': None, 'count_tap': 0}
-hide = [True] * 64
+hide = [True] * 16
 
 
 def square(x, y):
@@ -28,19 +28,19 @@ def square(x, y):
     color('black', 'white')
     begin_fill()
     for count in range(4):
-        forward(50)
+        forward(100)
         left(90)
     end_fill()
 
 
 def index(x, y):
     """Convert (x, y) coordinates to tiles index."""
-    return int((x + 200) // 50 + ((y + 200) // 50) * 8)
+    return int((x + 200) // 100 + ((y + 200) // 100) * 4)
 
 
 def xy(count):
     """Convert tiles count to (x, y) coordinates."""
-    return (count % 8) * 50 - 200, (count // 8) * 50 - 200
+    return (count % 4) * 100 - 200, (count // 4) * 100 - 200
 
 
 def tap(x, y):
@@ -69,7 +69,7 @@ def draw():
     shape(car)
     stamp()
 
-    for count in range(64):
+    for count in range(16):
         if hide[count]:
             x, y = xy(count)
             square(x, y)
